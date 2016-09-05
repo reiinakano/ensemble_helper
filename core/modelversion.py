3a5e1d19059023eb7b305986515060a4a7e1ecec
reiinakano <reii_nakano@yahoo.com>
2016-09-05 14:49:01 +0800

    relocation

---
+++ core/modelversion.py
@@ -0,0 +1,6 @@
+# This file contains the class to contain a single model version and its metadata.
+
+
+class ModelVersion:
+    def __init__(self):
+        pass
---
+++ core/modulemanager.py
@@ -0,0 +1,4 @@
+# This file contains the interface for interacting with the models and scorers in the packages enModels and enScorers
+
+class ModuleManager:
+    def __init__(self):
