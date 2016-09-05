0ba6c28d6706238bf504c82085ea35eee17668ae
reiinakano <reii_nakano@yahoo.com>
2016-09-05 22:25:07 +0800

    changes for modulemanager

--- core/modulemanager.py
+++ core/modulemanager.py
@@ -1,5 +1,5 @@
 # This file contains the interface for interacting with the models and scorers in the packages enModels and enScorers
-import pkgutil
+import os, importlib
 
 
 class ModuleManager:
@@ -11,6 +11,13 @@ class ModuleManager:
 
     def _get_available_models(self):
         available_models = {}
+        for dir_name in [f for f in os.listdir('./enModels') if os.path.isdir('./enModels/' + f)]:
+            # insert instructions to test if path is a legitimate model package
+            mod_name = 'enModels.' + dir_name + '.modelclass'
+            imported_module = importlib.import_module(mod_name)
+            my_class = getattr(imported_module, "ModelClass")
+            model_name = my_class.model_name()
+
 
         return available_models
 
@@ -21,3 +28,14 @@ class ModuleManager:
 
 
 if __name__ == "__main__":
+    print ['./enScorers/' + f for f in os.listdir('./enScorers') if os.path.isdir('./enScorers/' + f)]
+    print ['./enModels/' + f for f in os.listdir('./enModels') if os.path.isdir('./enModels/' + f)]
+    for dir_name in [f for f in os.listdir('./enModels') if os.path.isdir('./enModels/' + f)]:
+        # insert instructions to test if path is a legitimate model package
+        mod_name = 'enModels.' + dir_name + '.modelclass'
+        print mod_name
+        imported_module = importlib.import_module(mod_name)
+        print imported_module
+        my_class = getattr(imported_module, "ModelClass")
+        print my_class
+        print my_class.model_name()
