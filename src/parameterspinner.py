744eab7731f8d1b898a2cddad61bd1f3c7fb454a
reiinakano <reii_nakano@yahoo.com>
2016-09-08 05:06:32 +0800

    haha such cheating. added exhaustive search iterator using sklearn.grid_search.ParameterGrid

--- src/parameterspinner.py
+++ src/parameterspinner.py
@@ -1,5 +1,7 @@
 # This module contains the class for a parameter spinner, containing different methods to automatically generate valid
 # hyperparameters from a hyperparameter information dictionary i.e. dictionary "hyperparam" in a hyperparam.py file,
+from sklearn.grid_search import ParameterGrid
+from collections import Mapping
 
 
 class ParameterSpinner:
@@ -16,13 +18,15 @@ class ParameterSpinner:
     def use_default_values(hyperdict):
         return {key: value["default"] for key, value in hyperdict.iteritems()}
 
-    # This function is very similar in function to the exhaustive grid search using sklearn's GridSearchCV.
+    # This function is pretty much the exhaustive grid search using sklearn's GridSearchCV.
     # "params" is a list of dictionaries representing various grids to be generated.
     # The function returns an iterator that yields a valid hyperparam dictionary per iteration.
     @staticmethod
     def exhaustive_search_iterator(hyperdict, param_grid):
+        if isinstance(param_grid, Mapping):
+            param_grid = [param_grid]
         for grid in param_grid:
-
+            pass
 
 
 if __name__ == "__main__":
@@ -30,3 +34,5 @@ if __name__ == "__main__":
     m = modulemanager.ModuleManager()
     hyperdictt = m.get_model_hyperparams("Logistic Regression")
     print ParameterSpinner.use_default_values(hyperdictt)
+    for g in ParameterGrid([{"d":[2,3], "w":[2,1,3,3]},{"C":[2, 4], "f":[2, "str"]}]):
+        print g
