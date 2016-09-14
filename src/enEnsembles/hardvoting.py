d816210c80ce992c84de94dee9d3444e494b7d1d
reiinakano <reii_nakano@yahoo.com>
2016-09-15 05:33:50 +0800

    voting classifier (hard). no scoring method yet.

--- src/enEnsembles/hardvoting.py
+++ src/enEnsembles/hardvoting.py
@@ -1,11 +1,55 @@
-# This module contains the class for implementing a hard voting (uses majority classification, not averaging) ensemble.
+# This module contains the class for implementing a hard voting (uses majority classification, not average voting)
+# ensemble.
 
 
 class HardVotingClassifier:
-    def __init__(self, parent_set, scorer_name, scorer_hyperparam, module_mgr):
+    def __init__(self, parent_set, module_mgr):
         self.parent_set = parent_set
-        self.scorer_name = scorer_name
-        self.scorer_hyperparam = scorer_hyperparam
-        self.scorer_func = module_mgr.get_scorer_func(scorer_name)
-        self.scores = {}
-        self.models = {}
+        self.module_mgr = module_mgr
+        self.scores = {}  # scores dictionary
+        self._modelversions = set([])  # contains the ModelVersions that are in the HardVotingClassifier
+        self.user_notes = ""
+        self.scored = False
+
+    def add_model(self, modelversion):
+        self._modelversions.add(modelversion)
+
+    def score(self, scorer_name, scorer_hyperparam):
+        pass
+
+    def train(self):
+        for modelversion in sorted(self._modelversions):
+            if not modelversion.trained_model:
+                modelversion.train()
+
+    def predict(self, outside_set):
+        for modelversion in sorted(self._modelversions):
+            if not modelversion.trained_model:
+                print "Not all models in ensemble are trained."
+                return None
+        classifications = [modelversion.predict(outside_set) for modelversion in sorted(self._modelversions)]
+        classifications = get_majority(classifications)
+        return classifications
+
+
+# Sample use case:
+# classifications = [["r","y","r","r"],["y","r","r","r"],["y", "y", "y", "y",],["y","y","r","r"]]
+# get_majority(classifications) returns ["y","y","r","r"]
+def get_majority(classifications):
+    new_classifications = []
+    for votes in zip(*classifications):
+        my_dict = {}
+        for vote in votes:
+            if vote not in my_dict:
+                my_dict[vote] = 1
+            else:
+                my_dict[vote] += 1
+        new_classifications.append(max(my_dict, key=my_dict.get))
+    return new_classifications
+
+
+if __name__ == "__main__":
+    print get_majority([["r","y","r","r"],
+                        ["y","r","r","r"],
+                        ["y","y","y","y"],
+                        ["y","y","r","r"]])
