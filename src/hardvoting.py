830125a5548818bd4d13a88a74a07b90ec44f193
reiinakano <reii_nakano@yahoo.com>
2016-09-15 15:22:57 +0800

    hardvoting changes

--- src/hardvoting.py
+++ src/hardvoting.py
@@ -1,7 +1,7 @@
 # This module contains the class for implementing a hard voting (uses majority classification, not average voting)
 # ensemble.
-
-
+import parentset
+import featureextractor
 
 class HardVotingClassifier:
     def __init__(self, parent_set):
@@ -15,7 +15,15 @@ class HardVotingClassifier:
         self._modelversions.add(modelversion)
 
     def score(self, scorer_name, scorer_hyperparam, module_mgr):
-        pass
+        features = featureextractor.FeatureExtractor().get_features_array(self.parent_set)
+        labels = featureextractor.FeatureExtractor().get_labels_array(self.parent_set)
+        scorer_func = module_mgr.get_scorer_func(scorer_name)
+
+        scores = scorer_func(ForScorer(self), features, labels, **scorer_hyperparam)
+
+        self.scored = True
+        key = (scorer_name, tuple([value for key, value in sorted(scorer_hyperparam.iteritems())]))
+        self.scores[key] = scores
 
     def train(self):
         for modelversion in sorted(self._modelversions):
@@ -32,20 +40,27 @@ class HardVotingClassifier:
         return classifications
 
 
+# This class is used to play nice with the syntax of the scorers.
 class ForScorer:
     def __init__(self, hardvotingclassifier):
         self.models = []
         self.feature_extractors = []
-        self.parent_set = hardvotingclassifier.parent_set
         for modelversion in sorted(hardvotingclassifier._modelversions):
             self.models.append(modelversion.model_class(**modelversion.model_hyperparam))
             self.feature_extractors.append(modelversion.feature_extractor)
 
     def train(self, feature_set, labels):
+        parent_set = parentset.ParentSet(feature_set, labels)
         for model, feature_extractor in zip(self.models, self.feature_extractors):
-            model.train(feature_extractor.get_features_array(self.parent_set), feature_extractor.get_labels_array(self.parent_set))
+            model.train(feature_extractor.get_features_array(parent_set), feature_extractor.get_labels_array(parent_set))
 
-
+    def predict(self, feature_set_to_predict):
+        parent_set = parentset.ParentSet(feature_set_to_predict, None)
+        classifications = []
+        for model, feature_extractor in zip(self.models, self.feature_extractors):
+            classifications.append(model.predict(feature_extractor.get_features_array(parent_set)))
+        classifications = get_majority(classifications)
+        return classifications
 
 
 # Sample use case:
@@ -69,3 +84,5 @@ if __name__ == "__main__":
                         ["y","r","r","r"],
                         ["y","y","y","y"],
                         ["y","y","r","r"]])
+    import modulemanager
+    modulemanager.ModuleManager()
